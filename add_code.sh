b02c7318149e6656dd83374ad43a8901ee152754
Joseph Burgara <burgara@linux1.csc.calpoly.edu>
2026-09-13 19:54:25 -0700

    Update add_code.sh with field extraction and validation

--- add_code.sh
+++ add_code.sh
@@ -18,9 +18,9 @@ if [ ! -f "$target_file" ]; then
 fi
 
 file_owner=$(ls -l "$target_file" | awk '{print $3}')
-file_month=$(ls -l "$target_file" | awk '{print $6}')
-file_day=$(ls -l "$target_file" | awk '{print $7}')
-file_time=$(ls -l "$target_file" | awk '{print $8}')
+file_month=$(ls -l "$target_file" | awk '{print $7}')
+file_day=$(ls -l "$target_file" | awk '{print $8}')
+file_time=$(ls -l "$target_file" | awk '{print $9}')
 
 temp_file=$(mktemp)
 
